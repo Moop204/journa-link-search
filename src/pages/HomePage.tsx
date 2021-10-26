7b0f5a921a10461677f87546b8fccdb057bd1d93
Seng Yong Hoo <hoosyong@gmail.com>
2021-10-26 20:13:53 +1100

    trying to use fancy font for logo, found publisher logo images

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,4 +1,5 @@
-import { Grid, GridItem, Box, Center } from "@chakra-ui/react";
+import { Grid, GridItem, Box, Center, Text, Input, InputGroup, InputRightElement, Button, IconButton} from "@chakra-ui/react";
+import {SearchIcon} from '@chakra-ui/icons';
 import React, { FunctionComponent } from "react";
 
 const HomePage: FunctionComponent<{}> = () => {
@@ -6,34 +7,54 @@ const HomePage: FunctionComponent<{}> = () => {
   const supportedPublishers: number[] = [0,1,2,3,4,5,6,7,8,9];
 
   return (
-    <Grid templateColumns="repeat(2,1fr)" templateRows="repeat(2,1fr)" h="100vh">
-      <GridItem colSpan={1} bg="#d6b4b2">
-        test1
-      </GridItem>
-      <GridItem colSpan={1} bg="#a5c0d1">
-        test2
-      </GridItem>
+    <Box>
+      <Grid templateColumns="repeat(5,1fr)"h="100vh">
+        <GridItem colSpan={2} bg="#d6b4b2">
+          <Center h="100vh" pl="15vw">
+            Journalink
+          </Center>
+        </GridItem>
+        <GridItem colSpan={3} bg="#a5c0d1">
+          <Center h="100vh">
+            <InputGroup w="40vw">
+              <Input variant="outline" size="lg"/>
+              <InputRightElement h="47px">
+                <IconButton
+                  bg="rgb(a)"
+                  _hover={{ bg: "rgb(a)"}}
+                  icon={<SearchIcon />}
+                />
+              </InputRightElement>
+            </InputGroup>
+          </Center>
+        </GridItem>
       {/* supported publications section */}
-      <GridItem colSpan={2} bg="#a5d1ca">
-        <Center>
-          <Grid 
-            templateColumns="repeat(5,1fr)" 
-            templateRows="repeat(2,1fr)" 
-            gap={2}
-          >
-            {
-              supportedPublishers.map(pub => {
-                return (
-                  <GridItem colSpan={1} bg="white" w="80px" h="80px">
-                   {pub}
-                  </GridItem>
-                )
-              })
-            }
-          </Grid>
-        </Center>
-      </GridItem>
-    </Grid>
+      </Grid>
+      <Grid>
+        <GridItem colSpan={5} bg="#a5d1ca">
+          <Center>
+            <Grid 
+              templateColumns="repeat(5,1fr)" 
+              templateRows="repeat(3,1fr)" 
+              gap={2.5}
+            >
+              <GridItem colSpan={5} bg="white">
+                Supported Publications
+              </GridItem>
+              {
+                supportedPublishers.map(pub => {
+                  return (
+                    <GridItem colSpan={1} bg="white" w="65px" h="65px">
+                    <Center h="100%">{pub}</Center>
+                    </GridItem>
+                  )
+                })
+              }
+            </Grid>
+          </Center>
+        </GridItem>
+      </Grid>
+    </Box>
   )
 }
 
