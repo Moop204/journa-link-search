ef9b77eaf00021ea9c0952421df17c7ddcf8cdbb
Moop204 <andrew.nguyen2045@outlook.com>
2021-10-27 17:43:13 +1100

    [refactor](SearchForm.tsx) Removed unecessary lines.

--- src/SearchForm.tsx
+++ src/SearchForm.tsx
@@ -1,7 +1,5 @@
 import {
-  Center,
   Input,
-  Stack,
   Button,
   InputGroup,
   InputRightElement,
@@ -151,32 +149,3 @@ export const SearchForm: FunctionComponent<{}> = () => {
     </>
   );
 };
-
-// const SearchForm: FunctionComponent<{}> = () => {
-//   return (
-//     <>
-//       <Center>
-//         <Input
-//           placeholder="Search here!"
-//           bg="white.200"
-//           width="40%"
-//           textAlign="center"
-//           textColor="white"
-//           textDecorationColor="white"
-//         />
-//       </Center>
-//       <Center>
-//         <Stack direction="row" spacing={4} align="center">
-//           <Button colorScheme="teal" variant="solid">
-//             Reporter
-//           </Button>
-//           <Button colorScheme="teal" variant="solid">
-//             Publisher
-//           </Button>
-//         </Stack>
-//       </Center>
-//     </>
-//   );
-// };
-
-// export { SearchForm };
